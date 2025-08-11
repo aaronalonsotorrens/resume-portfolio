55fce1d73d2bf36a29f573e2de30a3e53671362a
aaron.aaronalonsotorrens@gmail.com <aaronalonsotorrens@macbookpro.home>
2025-08-11 22:46:59 +0100

    Update emailjs and added CV to its respective link

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,16 +1,22 @@
 function sendMail(contactForm) {
-    emailjs.send("service_jia8kwc", "Aaron's portfolio", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.projectsummary.value
+  emailjs
+    .send("service_jia8kwc", "Aaron's portfolio", {
+      from_name: contactForm.name.value,
+      from_email: contactForm.emailaddress.value,
+      project_request: contactForm.projectsummary.value,
     })
     .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
+      function (response) {
+        console.log("SUCCESS", response);
+        contactForm.reset(); // Clears form
+        document.getElementById("form-message").textContent =
+          "Your message has been sent!";
+      },
+      function (error) {
+        console.log("FAILED", error);
+        document.getElementById("form-message").textContent =
+          "Oops! Something went wrong. Please try again.";
+      }
     );
-    return false;  // To block from loading a new page
+  return false;
 }
